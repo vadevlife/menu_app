5736f7314a39adbf068a6b7102169e31af1bf7ae
vadevlife <vadevlife@gmail.com>
2022-07-01 17:41:16 -0300

    ...

---
+++ teste.js
@@ -0,0 +1,259 @@
+ 
+
+
+  const allProds = [{  
+     categoria :"Hot Dogs",
+    "itens":[
+      { 
+        id:'0',
+        name:'Cão Quente',
+        description:'Purê de batatas, tomate, milho, batata palha, 2 salsichas, catupiry e cheddar.',
+        price:'R$ 14,00',
+        img:'thumb.png',
+        quantidade: 0,
+      },{ 
+        id:'1',
+        name:'Hot Dog com Parmesão',
+        description:'Purê de batatas, tomate,  batata palha, salsicha, molho de tomate e queijo ralado.',
+        price:'R$ 13,00',
+        img:'thumb.png',
+        quantidade: 0,
+      },{ 
+        id:'2',
+        name:'Hot Dog Misto',
+        description:'Purê de batatas, tomate, milho, batata palha, salsicha, presunto, queijo, catupiry e cheddar.',
+        price:'R$ 17,00',
+        img:'thumb.png',
+        quantidade: 0,
+      },{ 
+        id:'3',
+        name:'Big Hot Dog',
+        description:'Purê de batatas, tomate, milho, batata palha, triplo salsicha, presunto, queijo, catupiry e cheddar.',
+        price:'R$ 19,00',
+        img:'thumb.png',
+        quantidade: 0,
+      },{ 
+        id:'4',
+        name:'Hot Calabacon',
+        description:'Purê de batatas, tomate, milho, batata palha, salsicha, calabresa, bacon,  catupiry e cheddar.',
+        price:'R$ 23,00',
+        img:'thumb.png',
+        quantidade: 0,
+      }
+  ]
+  },
+  {  
+    categoria:"Hambúrguers",
+    "itens":[
+      { 
+        id:'5',
+        name:'Hambúrguer',
+        description:'Purê de batatas, tomate, milho, batata palha, hambúrguer, presunto, queijo, catupiry e cheddar.',
+        price:'R$ 16,00',
+        img:'hamburguer.png',
+        quantidade: 0,
+      },{ 
+        id:'6',
+        name:'X Burguer',
+        description:'Purê de batatas, batata palha, hambúrguer, queijo, catupiry e cheddar.',
+        price:'R$ 18,00',
+        img:'hamburguer.png',
+        quantidade: 0,
+      },{ 
+        id:'7',
+        name:'X Salada Hambúrguer Tradicional',
+        description:'Purê de batatas, alface, tomate, milho, batata palha, hambúrguer, presunto, queijo, catupiry e cheddar.',
+        price:'R$ 20,00',
+        img:'hamburguer.png',
+        quantidade: 0,
+      },{ 
+        id:'8',
+        name:'X Salada Hambúrguer Tradicional',
+        description:'Purê de batatas, alface, tomate, milho, batata palha, hambúrguer, presunto, queijo, catupiry e cheddar.',
+        price:'R$ 19,00',
+        img:'hamburguer.png',
+        quantidade: 0,
+      },{ 
+        id:'9',
+        name:'X Egg',
+        description:'Purê de batatas, alface, tomate, milho, batata palha, hambúrguer, ovo, queijo, catupiry e cheddar.',
+        price:'R$ 21,00',
+        img:'hamburguer.png',
+        quantidade: 0,
+      },{ 
+        id:'10',
+        name:'X Bacon',
+        description:'Purê de batatas, alface, tomate, milho, batata palha, hambúrguer, bacon, queijo, catupiry e cheddar.',
+        price:'R$ 23,00',
+        img:'hamburguer.png',
+        quantidade: 0,
+      },{ 
+        id:'11',
+        name:'X Frango',
+        description:'Purê de batatas, alface, tomate, milho, batata palha, hambúrguer, filé de frango, queijo, catupiry e cheddar.',
+        price:'R$ 23,00',
+        img:'hamburguer.png',
+        quantidade: 0,
+      },{ 
+        id:'12',
+        name:'Americano',
+        description:'Purê de batatas, alface, tomate, milho, batata palha, hambúrguer, filé de frango, queijo, catupiry e cheddar.',
+        price:'R$ 20,00',
+        img:'hamburguer.png',
+        quantidade: 0,
+      },{ 
+        id:'13',
+        name:'Americano com Hambúrguer',
+        description:'Purê de batatas, alface, tomate, milho, batata palha, hambúrguer, ovo, queijo, catupiry e cheddar.',
+        price:'R$ 23,00',
+        img:'hamburguer.png',
+        quantidade: 0,
+      },{ 
+        id:'14',
+        name:'Misto Quente',
+        description:'Purê de batatas, batata palha, presunto, queijo, catupiry e cheddar.',
+        price:'R$ 16,00',
+        img:'hamburguer.png',
+        quantidade: 0,
+      },{ 
+        id:'15',
+        name:'Bauru',
+        description:'Purê de batatas, alface, tomate, milho, batata palha, presunto, queijo, catupiry e cheddar.',
+        price:'R$ 18,00',
+        img:'hamburguer.png',
+        quantidade: 0,
+      },{ 
+        id:'16',
+        name:'X Churrasco',
+        description:'Purê de batatas, alface, tomate, milho, batata palha, contra filé, queijo, catupiry e cheddar.',
+        price:'R$ 26,00',
+        img:'hamburguer.png',
+        quantidade: 0,
+      },{ 
+        id:'17',
+        name:'X Tudo',
+        description:'Purê de batatas, alface, tomate, milho, batata palha, hambúrguer, salsicha, ovo, bacon, contra filé, filé de frango, calabresa, presunto, queijo, catupiry e cheddar.',
+        price:'R$ 51,00',
+        img:'hamburguer.png',
+        quantidade: 0,
+      }
+  ]  
+ },
+ {  
+  categoria:"Lanches Especiais",
+  "itens":[
+    { 
+      id:'18',
+      name:'Hambúrguer Gourmet com Bacon',
+      description:'Purê de batatas, alface, tomate, milho, batata palha, hambúrguer, bacon, queijo, catupiry e cheddar.',
+      price:'R$ 28,00',
+      img:'hamburguer.png',
+      quantidade: 0,
+    },{ 
+      id:'19',
+      name:'Hambúrguer Gourmet Pernil',
+      description:'Purê de batatas, alface, tomate, milho, batata palha, pernil desfiado, queijo, catupiry e cheddar.',
+      price:'R$ 25,00',
+      img:'hamburguer.png',
+      quantidade: 0,
+    },{ 
+      id:'20',
+      name:'Contra Filé',
+      description:'Purê de batatas, alface, tomate, milho, batata palha, contra filé, queijo, catupiry e cheddar.',
+      price:'R$ 25,00',
+      img:'hamburguer.png',
+      quantidade: 0,
+    },{ 
+      id:'21',
+      name:'X Salada Especial Hambúrguer Gourmt',
+      description:'Purê de batatas, alface, tomate, milho, batata palha, hambúrguer, presunto, queijo, catupiry e cheddar.',
+      price:'R$ 23,00',
+      img:'hamburguer.png',
+      quantidade: 0,
+    }
+  ]
+},
+{  
+  categoria:"Adicionais",
+  "itens":[
+    { 
+      id:'27',
+      name:'Hambúrguer',
+      description:'',
+      price:'R$ 5,00',
+      img:'hamburgueradc.png',
+      quantidade: 0,
+    },{ 
+      id:'28',
+      name:'Salsichas',
+      description:'',
+      price:'R$ 5,00',
+      img:'salsicha.png',
+      quantidade: 0,
+    },{ 
+      id:'29',
+      name:'Ovo',
+      description:'',
+      price:'R$ 5,00',
+      img:'ovos.png',
+      quantidade: 0,
+    },{ 
+      id:'30',
+      name:'Bacon',
+      description:'',
+      price:'R$ 5,00',
+      img:'bacon.png',
+      quantidade: 0,
+    },{ 
+      id:'31',
+      name:'Calabresa',
+      description:'',
+      price:'R$ 8,00',
+      img:'calabresa.png',
+      quantidade: 0,
+    }
+  ]
+},
+{  
+  categoria:"Bebidas",
+  "itens":[
+    { 
+      id:'22',
+      name:'Água',
+      description:'Sem Gás e com Gás',
+      price:'R$ 3,00',
+      img:'agua.png',
+      quantidade: 0,
+    },{ 
+      id:'23',
+      name:'Refrigerante Lata',
+      description:'Coca-Cola Lata, Guaraná de 1L.',
+      price:'R$ 6,00',
+      img:'refrilata.png',
+      quantidade: 0,
+    },{ 
+      id:'24',
+      name:'Suco Natural 500ml',
+      description:'Laranja, Limão, Maracujá. ',
+      price:'R$ 9,00',
+      img:'suconatural500.png',
+      quantidade: 0,
+    },{ 
+      id:'25',
+      name:'Suco DEL Valle',
+      description:'Laranja, Limão, Maracujá, Uva.',
+      price:'R$ 7,00',
+      img:'sucodelvalle.png',
+      quantidade: 0,
+    },{ 
+      id:'26',
+      name:'Caldo de Cana 500ml',
+      description:'',
+      price:'R$ 8,00',
+      img:'cana.png',
+      quantidade: 0,
+    }
+]  
+}]
+
+
